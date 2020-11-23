4398230ef583090da0e80a676b8aa927206e79c6
Stalight59 <redhawk3579@gmail.com>
2020-11-22 23:30:35 -0700

    Further web design changes

--- Public/Website Files/myFunctionality.js
+++ Public/Website Files/myFunctionality.js
@@ -1,5 +1,6 @@
 document.getElementById("body").style.paddingTop = document.getElementById("header").offsetHeight + "px";
 document.getElementById("body").style.paddingBottom = document.getElementById("footer").offsetHeight + "px";
+var windowWidth = window.innerWidth;
 
 function loadAboutMe() {
     clearSummaryImage();
@@ -8,59 +9,65 @@ function loadAboutMe() {
     + " I've worked at a coffee shop, web devlopment, and programming tutoring." 
     + " I graduated from high school with a 4.0, and am currently enrolled at SAIT with a 4.0 GPA."
     + " I love to read, play DnD, and swim.";
-    document.getElementById("summary-text").innerHTML = aboutMeText;
+    document.getElementById("summary").innerHTML = aboutMeText;
 
     let divSummary = document.getElementById("summary");
-
+    divSummary.style.textAlign = "left";
+    divSummary.style.width = "35%";
+    divSummary.style.marginTop = "50px";
     const imageContainer = document.createElement("div");
     divSummary.prepend(imageContainer);
-    const image = createImage("../images/Profile Picture.jpg", 115, 115);
-    image.style.marginRight = "5vh";
+
+    var image = 0;
+    if (windowWidth < 800) {
+        image = createImage("../images/Profile Picture.jpg", 400, 400);
+    }
+    else {image = createImage("../images/Profile Picture.jpg", 200, 200);}
+    image.style.marginRight = "20px";
     image.style.borderRadius = "20px";
+    image.style.float = "left";
     imageContainer.append(image);
 
     let root = document.getElementById("skills-list");
     while( root.firstChild ){
         root.removeChild( root.firstChild );
     }
-
-    document.getElementById("margin3").style.height = "0";
 }
 
 function loadPortfolio() {
     clearSummaryImage();
+    resetSummary();
 
     let portfolioText = "As a first-year student at SAIT, I'm still learning all that I can about programming."
     + " However, I love to learn and I plan on picking up as many skills as I can."
     + " Here's a basic calculator app, along with a playable RPG game (both made in JavaScript)."
     + " I've also built this website.";
-    document.getElementById("summary-text").innerHTML = portfolioText;
-    
+    document.getElementById("summary").innerHTML = portfolioText;
+
     let root = document.getElementById("skills-list");
     while( root.firstChild ){
         root.removeChild( root.firstChild );
     }
 
     root.appendChild(createLink("#", "../images/Videogame.png", 
-    "../images/VideogameHover.png", 40, 30, "download-box", false, "Game"));
+    "../images/VideogameHover.png", 100, 75, "portfolioBox", false, "Game"));
 
     root.appendChild(createLink("../../Portfolio/Calculator/calc.html", "../images/Calculator.png",
-    "../images/CalculatorHover.png", 36, 46, "download-box", false));
+    "../images/CalculatorHover.png", 100, 125, "portfolioBox", false));
 
     root.appendChild(createLink("../../Portfolio/Pen Website/pen.html", "../images/Website.png",
-    "../images/Website Hover.png", 32, 32, "download-box", false, "Website"));
-
-    document.getElementById("margin3").style.height = "30px";
+    "../images/Website Hover.png", 80, 80, "portfolioBox", false, "Website"));
 }
 
 function loadResumeSummary() {
     clearSummaryImage();
+    resetSummary();
 
     let resumeSummary = "Software Development Student at SAIT with leadership experience, web development knowledge"
     + " and general programming skills. I've completed a website (working on two others) and a videogame written"
     + " in both Java and JavaScript. I have a passion for programming and I'm looking to further my skills"
     + " in an internship position.";
-    document.getElementById("summary-text").innerHTML = resumeSummary;
+    document.getElementById("summary").innerHTML = resumeSummary;
     
     let root = document.getElementById("skills-list");
     while( root.firstChild ){
@@ -69,11 +76,11 @@ function loadResumeSummary() {
 
     /*Creating Resume icon and adding to ul*/
     root.appendChild(createLink("../../Portfolio/Resume.pdf", "../images/ResumeDownload.png",
-    "../images/ResumeHover.png", 30, 30, "download-box", true, "Resume"));
+    "../images/ResumeHover.png", 75, 75, "resumeBox", true, "Resume"));
 
     /*Creating CV icon and adding to ul*/
     root.appendChild(createLink("../../Portfolio/Resume.pdf", "../images/CVDownload.png", 
-    "../images/CVHover.png", 40, 46, "download-box", true));
+    "../images/CVHover.png", 100, 120, "resumeBox", true));
 
     document.getElementById("margin3").style.height = "30px";
 }
@@ -84,7 +91,6 @@ function createIcon(src, width, height, boxType, text) {
 
     /*Creating div container and adding to li*/
     const iconContainer = document.createElement("div");
-    iconContainer.classList.add("skills");
     iconDownload.appendChild(iconContainer);
 
     /*Creating image element and adding to div container*/
@@ -102,8 +108,6 @@ function createLink(link, imgSrc, hoverSrc, width, height, boxType, isDownload, 
 
     /*Creating div container and adding to li*/
     const iconContainer = document.createElement("a");
-    iconContainer.classList.add("skills");
-    iconContainer.classList.add("remove-text-decoration");
     iconContainer.href = link;
     if (isDownload==true){
         iconContainer.setAttribute('download',"download=" + text);
@@ -150,4 +154,17 @@ return false;
 
 function isImage(i) {
     return i instanceof HTMLImageElement;
+}
+
+function resetSummary() {
+    var summary = document.getElementById("summary");
+    summary.style.textAlign = "center";
+    if (windowWidth < 800) {
+        summary.style.marginTop = "75px";
+        summary.style.width = "75%";
+    }
+    else {
+        summary.style.marginTop = "110px";
+        summary.style.width = "50%";
+    }
 }
